24a4b4cc387ea4a82907daa7d7de3e43b4e29c49
rohan80018 <rohanp8001@gmail.com>
2023-03-31 18:08:04 +0530

    updated score logic

--- src/App.js
+++ src/App.js
@@ -1,135 +1,4 @@
-// import React from "react";
-// import Die from "./Die";
-// import bear from "./images/bear.png";
-// import cat from "./images/cat.png";
-// import dog from "./images/dog.png";
-// import crab from "./images/crab.png";
-// import spider from "./images/spider.png";
-// import owl from "./images/owl.png";
-// import octopus from "./images/octopus.png";
-// import star from "./images/star.png";
-// import piegeon from "./images/piegeon.png";
-// import jelly from "./images/jelly.png";
-// import { nanoid } from "nanoid";
-// import Confetti from "react-confetti";
-
-// export default function App() {
-//     const [dice, setDice] = React.useState(allNewDice);
-//     const [tenzies, setTenzies] = React.useState(false);
-//     const [count, setCount] = React.useState(0)
-//     const [timeTaken, setTimeTaken] = React.useState(0);
-//     const [startTime, setStartTime] = React.useState(0);
-//     const [score, setScore] = React.useState(0);
-
-//     React.useEffect(() => {
-//         const allDiceHeld = dice.every((die) => die.isHeld);
-//         const firstValue = dice[0].value;
-//         const allSameValue = dice.every((die) => die.value === firstValue);
-//         if (allDiceHeld && allSameValue) {
-//         let time = Date.now() - startTime;
-//         setTimeTaken(Math.round(time / 1000));
-//         setTenzies(true);
-//         console.log(timeTaken, score);
-//         if (timeTaken < score) {
-//             setScore(Math.round(timeTaken));
-//         } else if (score === 0) {
-//             setScore(Math.round(timeTaken));
-//         } else if (timeTaken > score) {
-//             console.log("some");
-//         }
-//         }
-//     }, [dice,timeTaken]);
-//   // React.useEffect(()=> {
-//   //     const score = JSON.parse(localStorage.getItem('score'))
-//   //     if (score) {
-//   //         setScore(score)
-//   //     }
-//   // },[])
-
-//     function allNewDice() {
-//         let iconArray = [bear, cat,dog,crab,spider,owl,octopus,star,piegeon,jelly]
-//         const newDice = [];
-//         for (let i = 0; i < 10; i++) {
-//         newDice.push({
-//             value: iconArray[Math.floor(Math.random() * iconArray.length)],
-//             isHeld: false,
-//             id: nanoid(),
-//         });
-//         }
-//         return newDice;
-//     }
-
-//     function rollDice() {
-//         if (!tenzies) {
-//         let iconArray = [bear, cat,dog ,crab,spider,owl,octopus,star,piegeon,jelly]
-//         setCount(count + 1);
-//         setDice((oldDice) =>
-//             oldDice.map((die) => {
-//             return die.isHeld
-//                 ? die
-//                 : {
-//                     value: iconArray[Math.floor(Math.random() * iconArray.length)],
-//                     isHeld: false,
-//                     id: nanoid(),
-//                 };
-//             })
-//         );
-//         } else {
-//         setCount(0);
-//         setStartTime(0);
-//         setTenzies(false);
-//         setDice(allNewDice());
-//         }
-//     }
-
-//     function holdDice(id) {
-//         // console.log(count)
-//         if (count === 0 && startTime === 0) {
-//         setStartTime(Date.now());
-//         }
-//         setDice((oldDice) =>
-//         oldDice.map((die) => {
-//             return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-//         })
-//         );
-//         // console.log(startTime)
-//     }
-
-//     const diceElements = dice.map((die) => (
-//         <Die
-//         key={die.id}
-//         value={die.value}
-//         isHeld={die.isHeld}
-//         holdDice={() => holdDice(die.id)}
-//         />
-//     ));
-//     const timer = <span>{score}</span>
-//     return (
-//         <main>
-//         {tenzies && <Confetti />}
-//         <h1 className="title">Tenzies With Animals</h1>
-//         {tenzies ? (
-//             <p> Current Session Top Score <span>{Math.round(score)}</span> sec</p>
-//         ) : (
-//             <p className="instruction">
-//             Roll until all animals are the same. Click each die to freeze it at
-//             its current value between rolls
-//             </p>
-//         )}
-//         <div className="dice-container">{diceElements}</div>
-//         {tenzies ? (<p>Time Taken <span>{timeTaken}</span> sec</p>) : ""}
-//         <p>You have rolled <span>{count}</span> times</p>
-//         <button className="roll-dice" onClick={rollDice}>
-//             {tenzies ? "New Game" : "Roll"}
-//         </button>
-//         </main>
-//     );
-//     }
-
-
-
-
-import React from "react";
+import {useState, useEffect} from "react";
 import Die from "./Die";
 import bear from "./images/bear.png";
 import cat from "./images/cat.png";
@@ -145,40 +14,30 @@ import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 
 export default function App() {
-    const [dice, setDice] = React.useState(allNewDice);
-    const [tenzies, setTenzies] = React.useState(false);
-    const [count, setCount] = React.useState(0)
-    const [timeTaken, setTimeTaken] = React.useState(0);
-    const [startTime, setStartTime] = React.useState(0);
-    const [score, setScore] = React.useState(localStorage.getItem('score')?JSON.parse(localStorage.getItem('score')): 0);
-    // console.log(score)
-    React.useEffect(() => {
+    const [dice, setDice] = useState(allNewDice);
+    const [tenzies, setTenzies] = useState(false);
+    const [count, setCount] = useState(0)
+    const [timeTaken, setTimeTaken] = useState(0);
+    const [startTime, setStartTime] = useState(0);
+    const [score, setScore] = useState(localStorage.getItem('score')?JSON.parse(localStorage.getItem('score')): 0);
+
+    useEffect(() => {
         const allDiceHeld = dice.every((die) => die.isHeld);
         const firstValue = dice[0].value;
         const allSameValue = dice.every((die) => die.value === firstValue);
         if (allDiceHeld && allSameValue) {
-        let time = Date.now() - startTime;
-        setTimeTaken(Math.round(time / 1000));
-        setTenzies(true);
-        // console.log(timeTaken, score);
-        if (timeTaken < score) {
-            // console.log("enter")
-            setScore(Math.round(timeTaken))
-            localStorage.setItem('score',JSON.stringify(timeTaken));
-        } else if (score === 0) {
-            setScore(Math.round(timeTaken))
-            localStorage.setItem('score',JSON.stringify(timeTaken));
-        } else if (timeTaken > score) {
-            // console.log("some");
-        }
+            let time = Date.now() - startTime;
+            setTimeTaken(Math.round(time / 1000));
+            setTenzies(true);
+            if (timeTaken < score) {
+                setScore(Math.round(timeTaken))
+                localStorage.setItem('score',JSON.stringify(timeTaken));
+            } else if (score === 0) {
+                setScore(Math.round(timeTaken))
+                localStorage.setItem('score',JSON.stringify(timeTaken));
+            }
         }
     }, [dice,timeTaken]);
-  // React.useEffect(()=> {
-  //     const score = JSON.parse(localStorage.getItem('score'))
-  //     if (score) {
-  //         setScore(score)
-  //     }
-  // },[])
 
     function allNewDice() {
         let iconArray = [bear, cat, dog , crab, spider, owl, octopus, star, piegeon, jelly]
@@ -196,31 +55,31 @@ export default function App() {
 
     function rollDice() {
         if (!tenzies) {
-        let iconArray = [bear, cat, dog , crab, spider, owl, octopus, star, piegeon, jelly]
-        setCount(count + 1);
-        setDice((oldDice) =>
-            oldDice.map((die) => {
-            return die.isHeld
-                ? die
-                : {
-                    value: iconArray[Math.floor(Math.random() * iconArray.length)],
-                    isHeld: false,
-                    id: nanoid(),
-                };
-            })
-        );
+            let iconArray = [bear, cat, dog , crab, spider, owl, octopus, star, piegeon, jelly]
+            setCount(count + 1);
+            setDice((oldDice) =>
+                oldDice.map((die) => {
+                return die.isHeld
+                    ? die
+                    : {
+                        value: iconArray[Math.floor(Math.random() * iconArray.length)],
+                        isHeld: false,
+                        id: nanoid(),
+                    };
+                })
+            );
         } else {
-        setCount(0);
-        setStartTime(0);
-        setTenzies(false);
-        setDice(allNewDice());
+            setCount(0);
+            setStartTime(0);
+            setTenzies(false);
+            setDice(allNewDice());
         }
     }
 
     function holdDice(id) {
         // console.log(count)
         if (count === 0 && startTime === 0) {
-        setStartTime(Date.now());
+            setStartTime(Date.now());
         }
         setDice((oldDice) =>
         oldDice.map((die) => {
